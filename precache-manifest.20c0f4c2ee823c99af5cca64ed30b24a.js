eb89900867359e8960ba408f46026d2ce177a47a
Athul Muralidharan <athulmurali@gmail.com>
2019-01-18 23:52:09 -0500

    Updates

---
+++ precache-manifest.20c0f4c2ee823c99af5cca64ed30b24a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "82f15f6f6a20eaf995c1",
+    "url": "/static/css/main.b7865d1a.chunk.css"
+  },
+  {
+    "revision": "82f15f6f6a20eaf995c1",
+    "url": "/static/js/main.82f15f6f.chunk.js"
+  },
+  {
+    "revision": "65b3faedcfe36ab3c237",
+    "url": "/static/css/1.e05f9757.chunk.css"
+  },
+  {
+    "revision": "65b3faedcfe36ab3c237",
+    "url": "/static/js/1.65b3faed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "12aaf51f7857193743560026c537651e",
+    "url": "/index.html"
+  }
+];
