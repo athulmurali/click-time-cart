79cd2998d95d8e0baf68ecf2d27a246271c9a5c3
Athul Muralidharan <athulmurali@gmail.com>
2019-01-24 13:54:31 -0500

    Updates

---
+++ precache-manifest.ae8988eb6d2cdb2a3e6708afe6532f5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "52b939992cfe6cd5863f",
+    "url": "/click-time-cart/static/css/main.83163be1.chunk.css"
+  },
+  {
+    "revision": "52b939992cfe6cd5863f",
+    "url": "/click-time-cart/static/js/main.52b93999.chunk.js"
+  },
+  {
+    "revision": "30c94f321b9d7c348ef4",
+    "url": "/click-time-cart/static/css/1.bb11bd79.chunk.css"
+  },
+  {
+    "revision": "30c94f321b9d7c348ef4",
+    "url": "/click-time-cart/static/js/1.30c94f32.chunk.js"
+  },
+  {
+    "revision": "4423d4dae182907a2955",
+    "url": "/click-time-cart/static/js/runtime~main.4423d4da.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/click-time-cart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/click-time-cart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/click-time-cart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/click-time-cart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/click-time-cart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "bb62dbb204251f7e9a5f8b1a3fec3111",
+    "url": "/click-time-cart/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b9a29d163d7d724a21eacfbee5bce82.js"
+  "/click-time-cart/precache-manifest.ae8988eb6d2cdb2a3e6708afe6532f5f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/click-time-cart/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
